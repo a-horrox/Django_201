9ada4da8c1b210234cf897c64ce131530f30f9b4
a-horrox <84974314+a-horrox@users.noreply.github.com>
2021-06-30 18:51:17 +0100

    4. urls and views added

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+
+class HomeView(ListView):
+    http_method_names = ["get"]
+    template_name = "homeview.html"
+    model = Post
+    context_object_name = "post"
+    queryset = Post.objects.all().order_by('-id')[0:30]
+    
