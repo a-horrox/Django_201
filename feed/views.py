8f35fff7713bfcfa7a46e1b274d034b22f816099
a-horrox <84974314+a-horrox@users.noreply.github.com>
2021-07-01 14:02:47 +0100

    10. Modal added

--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
 
+# to learn more about views and django go to ccbv.co.uk
 
 class HomeView(ListView):
     http_method_names = ["get"]
@@ -9,3 +12,27 @@ class HomeView(ListView):
     model = Post
     context_object_name = "posts"
     queryset = Post.objects.all().order_by('-id')[0:30]
+
+class PostDetailView(DetailView):
+    http_method_names = ["get"]
+    template_name = 'feed/detail.html'
+    model = Post
+    context_object_name = 'post'
+
+class CreateNewPost(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'feed/create.html'
+    fields = ['text']
+    success_url ='/'
+    
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save
+        return super().form_valid(form)
+    
